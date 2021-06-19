f2941bea388aaae86f9806f9af0892392c1f404c
m1004 <tushinn12345@gmail.com>
2021-06-19 16:32:35 +0000

    taasklist

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
   get 'sessions/new'
-    root to: 'tasks#index'
-    get 'login', to: 'sessions#new'
-    post 'login', to: 'sessions#create'
-    delete 'logout', to: 'sessions#destroy'
+  root to: 'tasks#index'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
     
-    get 'signup', to: 'users#new'
-    resources :users, only: [:create]
-    resources :tasks
-    resources :tasklists, only: [:create, :destroy]
+  get 'signup', to: 'users#new'
+  resources :users, only: [:create]
+  resources :tasks
 end
